523f4f67972991d180f75535677c51fcaaf3f133
charu11 <charunumesh@gmail.com>
2020-04-02 20:57:07 +0530

    third commit

--- multi_processing.py
+++ multi_processing.py
@@ -5,7 +5,21 @@ def spawn():
     
 
 if __name__ == '__main__':
-    for i in range(5):
+    for i in range(100):
         p = multiprocessing.Process(target=spawn)
+        p.start()
+       # p.join()
+
+
+
+
+
+def spawn(num1, num2):
+    print('spawn # {}'.format(num1, num2))
+
+
+if __name__ == '__main__':
+    for i in range(5):
+        p = multiprocessing.Process(target=spawn, args=(i, i+1))
         p.start()
         p.join()
